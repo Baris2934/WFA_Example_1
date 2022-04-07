aab9c83365268fc186f89e2fe9d17e15ebfb938d
Barış Çelik <bariscelik294534@gmail.com>
2022-04-07 20:58:26 +0300

    Add project files.

---
+++ WFA_Example_1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace WFA_Example_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIslem1 = new System.Windows.Forms.Button();
+            this.lstSonuc = new System.Windows.Forms.ListBox();
+            this.btnIslem2 = new System.Windows.Forms.Button();
+            this.btnCift = new System.Windows.Forms.Button();
+            this.btnAlfabe = new System.Windows.Forms.Button();
+            this.btnToplam = new System.Windows.Forms.Button();
+            this.btnCarpim = new System.Windows.Forms.Button();
+            this.btnIslem3 = new System.Windows.Forms.Button();
+            this.btnYillar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIslem1
+            // 
+            this.btnIslem1.Location = new System.Drawing.Point(39, 37);
+            this.btnIslem1.Name = "btnIslem1";
+            this.btnIslem1.Size = new System.Drawing.Size(125, 38);
+            this.btnIslem1.TabIndex = 0;
+            this.btnIslem1.Text = "1-1000";
+            this.btnIslem1.UseVisualStyleBackColor = true;
+            this.btnIslem1.Click += new System.EventHandler(this.btnIslem1_Click);
+            // 
+            // lstSonuc
+            // 
+            this.lstSonuc.FormattingEnabled = true;
+            this.lstSonuc.Location = new System.Drawing.Point(245, 37);
+            this.lstSonuc.Name = "lstSonuc";
+            this.lstSonuc.Size = new System.Drawing.Size(408, 342);
+            this.lstSonuc.TabIndex = 1;
+            // 
+            // btnIslem2
+            // 
+            this.btnIslem2.Location = new System.Drawing.Point(39, 81);
+            this.btnIslem2.Name = "btnIslem2";
+            this.btnIslem2.Size = new System.Drawing.Size(125, 38);
+            this.btnIslem2.TabIndex = 0;
+            this.btnIslem2.Text = "1000-1";
+            this.btnIslem2.UseVisualStyleBackColor = true;
+            this.btnIslem2.Click += new System.EventHandler(this.btnIslem2_Click);
+            // 
+            // btnCift
+            // 
+            this.btnCift.Location = new System.Drawing.Point(39, 125);
+            this.btnCift.Name = "btnCift";
+            this.btnCift.Size = new System.Drawing.Size(125, 38);
+            this.btnCift.TabIndex = 0;
+            this.btnCift.Text = "1-1000 Çift";
+            this.btnCift.UseVisualStyleBackColor = true;
+            this.btnCift.Click += new System.EventHandler(this.btnCift_Click);
+            // 
+            // btnAlfabe
+            // 
+            this.btnAlfabe.Location = new System.Drawing.Point(39, 169);
+            this.btnAlfabe.Name = "btnAlfabe";
+            this.btnAlfabe.Size = new System.Drawing.Size(125, 38);
+            this.btnAlfabe.TabIndex = 0;
+            this.btnAlfabe.Text = "A\'dan Z\'ye";
+            this.btnAlfabe.UseVisualStyleBackColor = true;
+            this.btnAlfabe.Click += new System.EventHandler(this.btnAlfabe_Click);
+            // 
+            // btnToplam
+            // 
+            this.btnToplam.Location = new System.Drawing.Point(39, 213);
+            this.btnToplam.Name = "btnToplam";
+            this.btnToplam.Size = new System.Drawing.Size(125, 38);
+            this.btnToplam.TabIndex = 0;
+            this.btnToplam.Text = "1-100 Toplam";
+            this.btnToplam.UseVisualStyleBackColor = true;
+            this.btnToplam.Click += new System.EventHandler(this.btnToplam_Click);
+            // 
+            // btnCarpim
+            // 
+            this.btnCarpim.Location = new System.Drawing.Point(39, 257);
+            this.btnCarpim.Name = "btnCarpim";
+            this.btnCarpim.Size = new System.Drawing.Size(125, 38);
+            this.btnCarpim.TabIndex = 0;
+            this.btnCarpim.Text = "Çarpım Tablosu";
+            this.btnCarpim.UseVisualStyleBackColor = true;
+            this.btnCarpim.Click += new System.EventHandler(this.btnCarpim_Click);
+            // 
+            // btnIslem3
+            // 
+            this.btnIslem3.Location = new System.Drawing.Point(39, 301);
+            this.btnIslem3.Name = "btnIslem3";
+            this.btnIslem3.Size = new System.Drawing.Size(125, 38);
+            this.btnIslem3.TabIndex = 0;
+            this.btnIslem3.Text = "İşlem";
+            this.btnIslem3.UseVisualStyleBackColor = true;
+            this.btnIslem3.Click += new System.EventHandler(this.btnIslem3_Click);
+            // 
+            // btnYillar
+            // 
+            this.btnYillar.Location = new System.Drawing.Point(39, 345);
+            this.btnYillar.Name = "btnYillar";
+            this.btnYillar.Size = new System.Drawing.Size(125, 38);
+            this.btnYillar.TabIndex = 0;
+            this.btnYillar.Text = "Yıllar";
+            this.btnYillar.UseVisualStyleBackColor = true;
+            this.btnYillar.Click += new System.EventHandler(this.btnYillar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 431);
+            this.Controls.Add(this.lstSonuc);
+            this.Controls.Add(this.btnYillar);
+            this.Controls.Add(this.btnIslem3);
+            this.Controls.Add(this.btnCarpim);
+            this.Controls.Add(this.btnToplam);
+            this.Controls.Add(this.btnAlfabe);
+            this.Controls.Add(this.btnCift);
+            this.Controls.Add(this.btnIslem2);
+            this.Controls.Add(this.btnIslem1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIslem1;
+        private System.Windows.Forms.ListBox lstSonuc;
+        private System.Windows.Forms.Button btnIslem2;
+        private System.Windows.Forms.Button btnCift;
+        private System.Windows.Forms.Button btnAlfabe;
+        private System.Windows.Forms.Button btnToplam;
+        private System.Windows.Forms.Button btnCarpim;
+        private System.Windows.Forms.Button btnIslem3;
+        private System.Windows.Forms.Button btnYillar;
+    }
+}
+
---
+++ WFA_Example_1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_Example_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIslem1_Click(object sender, EventArgs e)
+        {
+            // 1 den 1000 e kadar olan sayıları (1000 dahil) ListBox'a ekleyin..
+           
+            
+            lstSonuc.Items.Clear();
+
+            for(int i = 1; i <= 1000; i++)
+            {
+                lstSonuc.Items.Add(i);
+            }
+        }
+
+        private void btnIslem2_Click(object sender, EventArgs e)
+        {
+            // 1000'den 1'e kadar olan sayıları (1 dahil) ListBox'a ekleyin..
+           
+            
+            lstSonuc.Items.Clear();
+
+            for(int i = 1000; i >= 1; i--)
+            {
+                lstSonuc.Items.Add(i);
+            }
+        }
+
+        private void btnCift_Click(object sender, EventArgs e)
+        {
+            // 1'den 1000'e kadar olan çift sayıları listeleyin (1000 dahil)..
+
+
+            lstSonuc.Items.Clear();
+
+            for(int i = 2; i <= 1000; i+=2)
+            {
+                lstSonuc.Items.Add(i);
+            }
+        }
+
+        private void btnAlfabe_Click(object sender, EventArgs e)
+        {
+            // A'dan Z'ye kadar olan harflerin kendileriyle birlikte yanlarına Ascii kodlarını yazarak ListBox'a ekleyin (sadece büyük harfler)..
+
+            lstSonuc.Items.Clear();
+
+            for (char c = 'A'; c <='Z'; c++)
+            {
+                lstSonuc.Items.Add($"{c}'nin Ascii kodu => {Convert.ToInt32(c)}");
+            }
+
+        }
+
+        private void btnToplam_Click(object sender, EventArgs e)
+        {
+            // 1-100 arasındaki sayıların (100 dahil) toplamını döngü ile bulup sonucu MessageBox ile gösterin..
+
+            int toplam = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                toplam += i;
+            }
+
+            MessageBox.Show(toplam.ToString());
+        }
+
+        private void btnCarpim_Click(object sender, EventArgs e)
+        {
+            // ListBox'ta bir çarpım tablosu şeması ekleyin..
+            /*
+            1x1=1
+            1x10=10
+            -----------
+            2x1=2
+            2x2=4
+            2x10=20
+            ---------
+
+             */
+
+            lstSonuc.Items.Clear();
+
+            for(int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    lstSonuc.Items.Add($"{i}x{j}={i * j}");
+                }
+
+                lstSonuc.Items.Add("-----------------");
+            }
+
+        }
+
+        private void btnIslem3_Click(object sender, EventArgs e)
+        {
+            // 1 ile 100 arasındaki (100 dahil) çift sayıların toplamı ile tek sayıların toplamının farkları kaçtır, MessageBox'ta gösterilsin..
+
+            int ciftlerinToplami = 0, teklerinToplami = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0) ciftlerinToplami += i;
+                else teklerinToplami += i;
+            }
+
+            int sonuc = ciftlerinToplami - teklerinToplami;
+
+            MessageBox.Show(sonuc.ToString());
+        }
+
+        private void btnYillar_Click(object sender, EventArgs e)
+        {
+            // 1945 ile günümüz  arasındaki yıllar ListBox'a eklensin ancak 1990 ve 1992 yılları eklenmesin..
+
+            lstSonuc.Items.Clear();
+
+            for (int i = 1945; i <= DateTime.Now.Year; i++)
+            {
+                if (i != 1990 && i != 1992) lstSonuc.Items.Add(i);
+            }
+        }
+    }
+}
